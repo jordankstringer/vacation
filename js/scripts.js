845720d76c87f7686f36a842af2c0c95362c8467
Jordan <jordankstringer@gmail.com>
2016-07-18 22:36:01 -0700

    remove some conditional statements and added image support

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,69 @@
 
 $(document).ready(function() {
   $("form#vacationAdvisor").submit(function(event) {
-  event.preventDefault();
-  $("#output").show();
+    event.preventDefault();
 
-  var q1 = document.getElementById("q1");
-  var q2 = document.getElementById("q2");
-  var q3 = document.getElementById("q3");
-  var q4 = document.getElementById("q4");
-  var q5 = document.getElementById("q5");
-  var camping = 0;
-  var mountains = 0;
-  var beach  = 0;
 
-  if (q1 === 0) {
-    camping++;
-}   else if (q1 === 1) {
-      mountains++;
-}     else (q1 === 2) {
+    var q1 = $("#q1").val();
+    var q2 = $("#q2").val();
+    var q3 = $("#q3").val();
+    var q4 = $("#q4").val();
+    var q5 = $("#q5").val();
+    var camping = 0;
+    var mountains = 0;
+    var beach  = 0;
+
+
+    if (q1 === 0) {
+      camping + 1;
+    } else if (q1 === 1) {
+        mountains + 1;
+    }  else {
+        beach + 1;
+    }
+
+    if (q2 === 0) {
+      camping++;
+    } else if (q2 === 1) {
+        mountains++;
+    }  else {
         beach++;
-}
+    }
 
-if (q2 === 0) {
-  camping++;
-}   else if (q2 === 1) {
-    mountains++;
-}     else (q2 === 2) {
-      beach++;
-}
+    if (q3 === 0) {
+      camping++;
+    } else if (q3 === 1) {
+        mountains++;
+    }  else {
+        beach++;
+    }
 
-if (q3 === 0) {
-  camping++;
-}   else if (q3 === 1) {
-    mountains++;
-}     else (q3 === 2) {
-      beach++;
-}
+    if (q4 === 0) {
+      camping++;
+    } else if (q4 === 1) {
+        mountains++;
+    }  else {
+        beach++;
+    }
 
-if (q4 === 0) {
-  camping++;
-}   else if (q4 === 1) {
-    mountains++;
-}     else (q4 === 2) {
-      beach++;
-}
+    if (q5 === 0) {
+      camping++;
+    } else if (q5 === 1) {
+        mountains++;
+    }  else {
+        beach++;
+    }
+    console.log('camping')
 
-if (q5 === 0) {
-  camping++;
-}   else if (q5 === 1) {
-    mountains++;
-}     else (q5 === 2) {
-      beach++;
-}
-
-if (camping === 5) {
-  $(".vacation").html('<a href="http://www.gocampingamerica.com/">Cancun</a>')
-}
+    if (camping >= 3) {
+      $("#camping").show()
+    } else if (mountains >= 3) {
+        $("#mountains").show()
+        else {
+          $("#beach").show()
+    }
 
 
-
+    $("#output").show();
   });
 });
